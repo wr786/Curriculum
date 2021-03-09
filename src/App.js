6fac6980cd3db09339ffc04cfed58463fcf6b791
wr786 <wr786@pku.edu.cn>
2021-03-09 15:46:18 +0800

    init the proj

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+class Course extends React.Component {
+  render() {
+    return (
+      <>
+        
+      </>
+    )
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
